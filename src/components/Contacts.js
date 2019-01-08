c821a485059054a10012a6695ead79dfd69da260
Josh Anderson <joshua.frederick.anderson@gmail.com>
2019-01-08 02:35:01 -0800

    first commit

---
+++ src/components/Contacts.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import Contact from './Contact'
+
+class Contacts extends Component {
+  state = {
+      contacts: [
+        {
+          id: 1,
+          name: 'John Doe',
+          email: 'jdoe@gmail.com',
+          phone: '555-555-555'
+        },
+        {
+          id: 2,
+          name: 'Dimmy Doe',
+          email: 'ddoe@gmail.com',
+          phone: '555-555-555'
+        },
+        {
+          id: 3,
+          name: 'Limmy Doe',
+          email: 'ldoe@gmail.com',
+          phone: '555-555-555'
+        }
+      ]
+    }
+
+
+  render() {
+    const { contacts } = this.state;
+
+    return (
+      <div>
+        {contacts.map(contact => (
+          <Contact
+          key={contact.id}
+          contact={contact}
+        />
+      ))}
+
+      </div>
+    )
+  }
+}
+
+export default Contacts;
